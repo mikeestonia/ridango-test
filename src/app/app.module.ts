a157676d5dcf846f92f66b48e4ee5979efa230f6
Mikk Visnap <mikk.visnap@gmail.com>
2021-03-14 09:15:37 +0200

    add routing, 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserListComponent } from './user-list/user-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
